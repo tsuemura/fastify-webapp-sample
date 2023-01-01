0657e9ed5c1ab02d978b4f7bbe41c594eb823f64
Takuya Suemura <takuyasuemura@gmail.com>
2023-01-02 01:36:25 +0900

    Implement session-based card system

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import fastify from "fastify";
 import view from "@fastify/view"
 import postgres from "@fastify/postgres"
+import connectPgSimple from "connect-pg-simple"
 import formbody from "@fastify/formbody"
+import session from "@fastify/session"
+import cookie from "@fastify/cookie"
 
 import ejs from "ejs"
 import * as dotenv from "dotenv"
 
 dotenv.config()
 const server = fastify();
+const pgSession = new connectPgSimple(session)
 
 server.register(view, {
   engine: {
@@ -15,12 +19,24 @@ server.register(view, {
   }
 })
 
+server.register(cookie)
+
 const connectionString = `postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}`
+
+// allow insecure cookie only during development
+server.register(session, {
+  secret: process.env.SESSION_SECRET,
+  store: new pgSession({
+    conString: connectionString,
+  }),
+  cookie: process.env.NODE_ENV == "development" ? { secure: false } : {},
+});
+
 
 server.register(formbody)
 
 server.register(postgres, {
-  connectionString,
+  connectionString
 })
 
 server.get("/items", async (request, reply) => {
@@ -28,14 +44,26 @@ server.get("/items", async (request, reply) => {
   const result = await client.query(
     "SELECT id, name, description, price FROM items"
   );
-  await reply.view("/src/views/items.ejs", { items: result.rows })
+
+  await reply.view("/src/views/items.ejs", {
+    items: result.rows,
+    itemsInCart: request.session.items
+  })
 })
 
 server.post("/items", async (request, reply) => {
-  const itemId = request.body.itemId
-  const quantity = request.body.quantity
+  const { itemId, quantity } = request.body
 
-  reply.send(`itemId: ${itemId}, quantity: ${quantity}`)
+  // 1. Initialize
+  request.session.items ??= {}
+
+  // 2. Current quantity of the itemId
+  request.session.items[itemId] ??= 0
+
+  // 3. Increase the quantity
+  request.session.items[itemId] += Number(quantity)
+
+  await reply.redirect(302, '/items')
 })
 
 server.listen({ port: 8080 }, (err, address) => {
