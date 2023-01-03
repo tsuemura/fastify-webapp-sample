30a03aa3c97e814629cfee27879fe2e6c751a9bd
Takuya Suemura <takuyasuemura@gmail.com>
2023-01-03 11:28:28 +0900

    Allow editing and modifying items to admin

--- src/index.js
+++ src/index.js
@@ -76,7 +76,8 @@ server.get("/items", async (request, reply) => {
 
   await reply.view("/src/views/items.ejs", {
     items: result.rows,
-    itemsInCart: request.session.items
+    itemsInCart: request.session.items,
+    user: request.user
   })
 })
 
@@ -100,7 +101,6 @@ server.get(
   {
     preValidation: passport.authenticate("local", { authInfo: false },
       (request, reply) => {
-        console.log(request.user)
         if (!request.user.isAdmin) {
           return reply.redirect(302, '/login')
         }
@@ -108,9 +108,71 @@ server.get(
     ),
   },
   async (request, reply) => {
-    await reply.send("you can see it!");
+    await reply.view('/src/views/addItem.ejs');
   }
 );
+
+server.post(
+  "/items/add",
+  {
+    preValidation: passport.authenticate("local", { authInfo: false },
+      (request, reply) => {
+        if (!request.user.isAdmin) {
+          return reply.redirect(302, '/login')
+        }
+      }
+    ),
+  },
+  async (request, reply) => {
+    const client = await server.pg.connect();
+    const { name, description, price } = request.body
+    const { rows } = await client.query("INSERT INTO items (name, description, price) VALUES ($1, $2, $3) RETURNING id, name, description, price", [name, description, price]);
+    await reply.view('/src/views/addItem.ejs', { item: rows[0] })
+  }
+);
+
+server.get("/items/:itemId/edit", {
+  preValidation: passport.authenticate(
+    "local",
+    { authInfo: false },
+    (request, reply) => {
+      if (!request.user.isAdmin) {
+        return reply.redirect(302, "/login");
+      }
+    }
+  )},
+  async (request, reply) => {
+    const client = await server.pg.connect();
+    const { itemId } = request.params
+    const { rows } = await client.query("SELECT id, name, description, price FROM items WHERE id = $1", [itemId])
+    const item = rows[0]
+    await reply.view('/src/views/editItem.ejs', {item, modified: false})
+  }
+);
+
+server.post("/items/:itemId/edit", {
+  preValidation: passport.authenticate(
+    "local",
+    { authInfo: false },
+    (request, reply) => {
+      if (!request.user.isAdmin) {
+        return reply.redirect(302, "/login");
+      }
+    }
+  )},
+  async (request, reply) => {
+    const client = await server.pg.connect();
+    const { itemId } = request.params
+    const { name, description, price } = request.body
+    const { rows } = await client.query(
+      "UPDATE items SET name = $2, description = $3, price = $4, updated_at = CURRENT_DATE WHERE id = $1 RETURNING id, name, description, price",
+      [itemId, name, description, price]
+    );
+    const item = rows[0]
+    await reply.view('/src/views/editItem.ejs', {item, modified: true})
+  }
+);
+
 
 server.get("/order", async (request, reply) => {
 
