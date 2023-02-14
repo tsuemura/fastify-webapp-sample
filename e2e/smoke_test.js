0fd3159ab2babf10dcefe483b47a1392dd508c3b
Takuya Suemura <takuyasuemura@gmail.com>
2023-02-14 13:51:52 +0900

    ログインのスモークテストケースを追加

--- e2e/smoke_test.js
+++ e2e/smoke_test.js
@@ -5,3 +5,11 @@ Scenario('Access to example.com', ({ I }) => {
   I.see('Example Domain')
 });
 
+Scenario('Open the website and login to it', ({ I }) => {
+  I.amOnPage("/"); // BASE_URLからの相対パスに書き換える
+  I.click("ログインする");
+  I.fillField("ユーザー名", "user1");
+  I.fillField("パスワード", "super-strong-passphrase");
+  I.click("ログイン");
+  I.see("user1 さん");
+})
