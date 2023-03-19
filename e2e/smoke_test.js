028935a43b6c1710b308a0c1fee89c3e3b7f8a73
Takuya Suemura <takuyasuemura@gmail.com>
2023-03-19 09:04:50 +0900

    注文番号に応じたボタンをクリックする

--- e2e/smoke_test.js
+++ e2e/smoke_test.js
@@ -5,7 +5,7 @@ Scenario('Access to example.com', ({ I }) => {
   I.see('Example Domain')
 });
 
-Scenario('Login and confirm order', ({ I }) => {
+Scenario('Login and confirm order', async ({ I }) => {
   I.amOnPage("/");
   I.click("ログインする");
   I.fillField("ユーザー名", "user1");
@@ -19,6 +19,7 @@ Scenario('Login and confirm order', ({ I }) => {
   I.fillField('受け取り日', '002023/08/01')
   I.fillField('受け取り目安時間', '12:00')
   I.click('注文を確定する')
+  const orderNo = await I.grabTextFrom('h3')
   session('お弁当屋さんのブラウザ', () => {
     I.amOnPage("/");
     I.click("ログインする");
@@ -26,8 +27,10 @@ Scenario('Login and confirm order', ({ I }) => {
     I.fillField("パスワード", "admin");
     I.click("ログイン");
     I.click("注文を管理する")
-    I.click("この注文を引き渡しました")
-    I.see("引き渡し済みの注文です")
+    within(locate('aside').withText(orderNo), () => {
+      I.click("この注文を引き渡しました")
+      I.see("引き渡し済みの注文です")
+    })
   })
 })
 
