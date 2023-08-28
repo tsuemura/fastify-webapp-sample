2f8bf47ba857b5ce46a2491e1369259f9579e186
Takuya Suemura <takuyasuemura@gmail.com>
2023-08-28 12:48:59 +0900

    テストケース名を修正

--- e2e/smoke_test.js
+++ e2e/smoke_test.js
@@ -1,11 +1,11 @@
-SuiteOf('Smoke test');
+SuiteOf('スモークテスト');
 
 Scenario('Access to example.com', ({ I }) => {
   I.amOnPage('https://example.com')
   I.see('Example Domain')
 });
 
-Scenario('Open the website and login to it', ({ I }) => {
+Scenario('Webサイトを開きログインする', ({ I }) => {
   I.amOnPage("/"); // BASE_URLからの相対パスに書き換える
   I.click("ログインする");
   I.fillField("ユーザー名", "user1");
