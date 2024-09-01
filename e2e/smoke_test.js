46a2ba63ae3cc2c9647c1c5a840565e445d44bd9
yuma <ikeoku.y@gmail.com>
2024-09-01 19:00:51 +0900

    URLを環境変数から渡すようにした

--- e2e/smoke_test.js
+++ e2e/smoke_test.js
@@ -1,7 +1,7 @@
 SuiteOf('スモークテスト');
 
 Scenario('Webサイトを開きログインする', ({ I }) => {
-  I.amOnPage('http://localhost:8080')
+  I.amOnPage('/')
   I.click('ログインする')
   I.fillField('ユーザー名', 'user1')
   I.fillField('パスワード', 'super-strong-passphrase')
