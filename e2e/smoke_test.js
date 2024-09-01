460c4b4031869a0c6c2531f901be21344864a0f6
yuma <ikeoku.y@gmail.com>
2024-09-01 18:43:38 +0900

    ログインのスモークテストケースを追加

--- e2e/smoke_test.js
+++ e2e/smoke_test.js
@@ -1,6 +1,10 @@
 SuiteOf('スモークテスト');
 
-Scenario('example.comにアクセスする',  ({ I }) => {
-  I.amOnPage('https://example.com')
-  I.see('Example Domain')
-});
+Scenario('Webサイトを開きログインする', ({ I }) => {
+  I.amOnPage('http://localhost:8080')
+  I.click('ログインする')
+  I.fillField('ユーザー名', 'user1')
+  I.fillField('パスワード', 'super-strong-passphrase')
+  I.click('ログイン')
+  I.see('user1 さん')
+})
