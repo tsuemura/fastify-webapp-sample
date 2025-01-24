5e490a2af9b7b19adbd0a75989166bf942500702
kazuhiro <moriyama.kazuhiro@earthsys-lab.co.jp>
2025-01-25 02:14:35 +0900

    ログインのスモールテストケースを追加

--- e2e/smoke_test.js
+++ e2e/smoke_test.js
@@ -1,6 +1,10 @@
 SuiteOf('smoke');
 
 Scenario('test something',  ({ I }) => {
- I.amOnPage('https://example.com')
- I.see('Example Domain')
+ I.amOnPage('https://fastify-webapp-sample.takuyasuemura.dev/')
+ I.click('ログインする')
+ I.fillField('ユーザー名', 'user1')
+ I.fillField('パスワード', 'super-strong-passphrase')
+ I.click('ログイン')
 });
+
